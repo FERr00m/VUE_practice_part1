afe5d1a0c38227b656c33803611ab15d1f9b9f86
Алексей Н. Мельников <u183@stekspb.ru>
2022-05-11 10:24:48 +0300

    router done

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView'
 import ForgetView from '@/views/ForgetView'
 import DashboardView from '@/views/DashboardView'
-import MailView from '@/views/MailView'
+// import MailView from '@/views/MailView'
+import AppEmailBody from '@/components/AppEmailBody'
+import ErrorView from '@/views/ErrorView'
 
 const routes = [
   {
@@ -14,26 +16,44 @@ const routes = [
   {
     path: '/forget',
     name: 'forget',
-    component: ForgetView
+    component: ForgetView,
+    meta: {
+      cantEnter: true
+    }
   },
   {
     path: '/dashboard',
     name: 'dashboard',
+    beforeEnter () {
+      console.log('beforeEnter')
+    },
     component: DashboardView
   },
   {
-    path: '/mail/:mailId?',
+    path: '/mail',
     name: 'mail',
-    component: MailView
+    children: [
+      {
+        path: ':mailId?',
+        component: AppEmailBody,
+        props: true
+      }
+    ],
+    component: () => import(/* webpackChunkName: "mail" */ '@/views/MailView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/:notFound(.*)',
+    name: '404',
+    component: ErrorView
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
@@ -41,6 +61,19 @@ const router = createRouter({
   routes,
   linkActiveClass: 'active',
   linkExactActiveClass: 'active'
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach')
+  if (to.meta.cantEnter) {
+    next({ name: 'dashboard' })
+  } else {
+    next()
+  }
+})
+
+router.afterEach((to, from) => {
+  //
 })
 
 export default router
